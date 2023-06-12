39a63f13ef45d1ae66eef21058a5a9f235614046
Rathu <rathankg@gmail.com>
2023-06-12 20:00:38 +0530

    added implementation for diff.java

--- diff.java
+++ diff.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {  
 int n3 = 225, n2 = 115, sum;  
 sum = n3 * n2;  
-System.out.println("The sum of numbers is : "+sum);  
+System.out.println(sum);  
 }  
 }  
